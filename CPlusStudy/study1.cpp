58825a135971bba45aa4ddbf4777901ecba59691
DongHyunLee98 <godsh88@gmail.com>
2023-04-19 19:16:07 +0900

    CPlus Study

---
+++ CPlusStudy/study1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main(void)
+{
+
+
+	// Study2();
+	// Study1();
+	return 0;
+}
+
+// Study 1일차
+void Study7()
+{
+
+}
+void Study6()
+{
+
+}
+void Study5()
+{
+
+}
+void Study4()
+{
+
+}
+void Study3()
+{
+
+}
+void Study2()
+{
+	int val1;
+	std::cout << "1번숫자 : ";
+	std::cin >> val1;
+
+	int val2;
+	std::cout << "2번숫자 : ";
+	std::cin >> val2;
+
+	int result = val1 + val2;
+	std::cout << val1 << val2 << result << std::endl;
+}
+void Study1()
+{
+	int num = 20;
+	std::cout << "Hellow World!" << std::endl;
+	std::cout << "Hello" << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+}
